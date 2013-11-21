e6d38f14c719afd141ff85e79cbf3f1dfac8e4e3
Naoya Ito <i.naoya@gmail.com>
2013-11-22 08:59:48 +0900

    First commit

---
+++ spec/list_spec.rb
@@ -0,0 +1,31 @@
+require 'list'
+
+describe List, "when empty" do
+  before do
+    @empty_list = []
+  end
+
+  it "should be empty" do
+    @empty_list.should be_empty
+  end
+
+  it "should size 0" do
+    @empty_list.size.should == 0
+  end
+
+  after do
+    @empty_list = nil
+  end
+end
+
+describe List, "when initialized with object" do
+  before do
+    # @list = List.new(3, Hash.new)
+    @list = List.new(3) { Hash.new }
+    @list[0][:cat] = "Nuko"
+  end
+
+  it "should not affect others" do
+    @list.should == [{:cat => "Nuko"}, {}, {}]
+  end
+end
