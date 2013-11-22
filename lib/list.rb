7eba926436df63a785b7089dc8856001534abf99
Naoya Ito <i.naoya@gmail.com>
2013-11-22 10:20:00 +0900

    Add first_index method to List

--- lib/list.rb
+++ lib/list.rb
@@ -1,2 +1,12 @@
 class List < Array
+  def first_index (&block)
+    return 0 if block.nil?
+
+    self.each_with_index do |item, index|
+      if block.call(item)
+        return index
+      end
+    end
+    nil
+  end
 end
